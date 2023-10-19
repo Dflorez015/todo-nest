ae715333e92ec643cec98a1220392ae0398aea87
Duvan Florez <dflorez@comercializadora-s3.com>
2023-10-19 11:13:00 -0500

    :tada: [INIT] to-do crud in memory

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }))
+
+  const config = new DocumentBuilder()
+    .setTitle('Task example')
+    .setDescription('The task API description')
+    .setVersion('1.0')
+    .addTag('task')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(8080);
+}
+bootstrap();
